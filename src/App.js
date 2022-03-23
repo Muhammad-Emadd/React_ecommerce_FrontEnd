3ba3f099c9c02cbcede91f7009626011a48249f5
Mohamed Emad <mohamed.emadd91@gmail.com>
2022-03-23 23:05:19 +0200

    work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import NavBar from "./Components/NavBar/NavBar";
+// import Trash from "./Components/try/trash";
+import Trash2 from "./Components/try/Trash2";
+import Body from "./Components/Body/Body";
+import CartPage from "./Components/CartPage/CartPage";
+// import { CATEGORIES } from "./GraphQL/Queries";
+
+const errorLink = onError(({ graphqlErrors, networkErrors }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      return alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:4000/graphql" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
+
+export class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        {/* <NavBar />
+        <Trash2 />
+        <Body /> */}
+
+        <CartPage />
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
+
+// client
+//   .query({
+//     query: CATEGORIES,
+//   })
+//   .then((result) => console.log(result));
